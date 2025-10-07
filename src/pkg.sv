a7495e526c416a614e2c4fc0979d6658234da01d
Apoorva shenoy <136146978+apoorvashenoy1203@users.noreply.github.com>
2025-10-07 23:27:27 +0530

    Create pkg.sv

---
+++ src/pkg.sv
@@ -0,0 +1,17 @@
+ package pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  `include "defines.sv"
+  `include "fifo_seq_item.sv"
+  `include "sequencer.sv"
+  `include "sequence.sv"
+  `include "write_driver.sv"
+  `include "write_monitor.sv"
+  `include "read_driver.sv"
+  `include "read_monitor.sv"
+  `include "fifo_agent.sv"
+  `include "fifo_scoreboard.sv"
+  `include "subscriber.sv"
+  `include "fifo_env.sv"
+  `include "test.sv"
+endpackage
